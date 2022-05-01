8e6835a5a2dff1f789322bc4b8137ca1f390df9c
Champak's World <champaksworld@gmail.com>
2022-05-01 21:39:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vsjurllauncher
+ 
---
+++ android/app/src/main/kotlin/com/blogspot/varanasisoftwarejunction/vsjurllauncher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.blogspot.varanasisoftwarejunction.vsjurllauncher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
